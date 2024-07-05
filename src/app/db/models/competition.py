fb88fcddeb3b11c1ccfa43580b76e510843c1ff1
Julien <courtes.julien@gmail.com>
2024-07-05 08:16:25 +0200

    chore: replaced prettier by biome

--- src/app/db/models/competition.py
+++ src/app/db/models/competition.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from datetime import datetime  # noqa: TCH003
 from typing import TYPE_CHECKING
 
 from advanced_alchemy.base import SlugKey, UUIDAuditBase
@@ -7,8 +8,6 @@ from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
-    from datetime import datetime
-
     from .division import Division
 
 
